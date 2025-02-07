35e91f23f9d6724b95b7fca01784b54e7c612046
jonayeds <jonayeeds@gmail.com>
2025-02-07 11:45:02 +0600

    generated static rendering  for selective blogs

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -1,4 +1,11 @@
 import BlogDetailsCard from "@/components/ui/BlogDetailsCard"
+import { Blog } from "@/types"
+
+export const generateStaticParams = async()=>{
+    const res = await fetch(`http://localhost:8000/blogs`)
+    const blogs = await res.json()
+    return blogs.slice(0,3).map((item:Blog)=>({blogId:item.id}))
+}
 
 
 const BlogDetailsPage =async ({params}:{params:Promise<{blogId:string}>}) => {
