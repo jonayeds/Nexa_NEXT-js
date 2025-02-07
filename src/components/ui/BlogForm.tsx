4e2d7b93420e1aef18e4623ad23956e1ce6cf94e
jonayeds <jonayeeds@gmail.com>
2025-02-07 19:10:31 +0600

    completed creating blog

--- src/components/ui/BlogForm.tsx
+++ src/components/ui/BlogForm.tsx
@@ -1,3 +1,4 @@
+import { createBlog } from '@/serverActions/createBlog';
 import Form from 'next/form'
 
 const BlogForm = () => {
@@ -7,7 +8,7 @@ const BlogForm = () => {
   return (
     <div className="flex items-center justify-center min-h-screen my-10">
       <div className="w-full max-w-4xl bg-white shadow-lg rounded-lg p-6">
-        <Form action={"/blogs"}>
+        <Form action={createBlog}>
           <h2 className="text-2xl font-semibold mb-6 text-teal-600 text-center">
             Create Blog
           </h2>
--- src/serverActions/createBlog.ts
+++ src/serverActions/createBlog.ts
@@ -1,13 +1,19 @@
-import { Blog } from "@/types"
+"use server"
 
-export const  createBlog = async(data:Blog)=>{
+import { redirect } from "next/navigation"
+
+export const  createBlog = async(data:FormData)=>{
+    const blogData = Object.fromEntries(data.entries())
     const res = await fetch("http://localhost:8000/blogs/",{
         method:"POST",
         headers:{
             "Content-type":"application/json"
         },
-        body:JSON.stringify(data)
+        body:JSON.stringify(blogData)
     })
     const blogInfo = await res.json()
+    if(blogInfo){
+        redirect(`/blogs/${blogInfo.id}`)
+    }
     return blogInfo
 }
