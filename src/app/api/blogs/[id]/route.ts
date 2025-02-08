d9f06400f90fe39854ee1b14314263990cf9be28
jonayeds <jonayeeds@gmail.com>
2025-02-08 21:18:07 +0600

    initiated redux and rtk query

--- src/app/api/blogs/[id]/route.ts
+++ src/app/api/blogs/[id]/route.ts
@@ -4,8 +4,6 @@ import { blogs } from "../route"
 
 export const GET = async(request:Request, {params}:{params:Promise<{id:string}>} )=>{
     const {id} = await params
-    console.log(id)
-    
     const blog = blogs.find(blog=>blog.id === id)
     return NextResponse.json(blog)
 }
